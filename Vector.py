83c0b2a7d97bd4aa4d01d2d2ce4d352f762964d9
Imran <imranyafith@gmail.com>
2022-03-03 00:55:53 -0500

    Added AngleBetween method

--- Vector.py
+++ Vector.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from math import sqrt
+import math
 
 
 class Vector2D:
@@ -12,13 +12,16 @@ class Vector2D:
         return (self.x * vector.x) + (self.y * vector.y)
 
     def Magnitude(self) -> float:
-        return sqrt((self.x ** 2) + (self.y ** 2))
+        return math.sqrt((self.x ** 2) + (self.y ** 2))
 
     def Normalized(self) -> Vector2D:
         return Vector2D((1/self.Magnitude())*(self.x), (1/self.Magnitude())*(self.y))
 
     def IsOrthogonal(self, vector: Vector2D) -> bool:
         return True if self.Dot(vector) == 0 else False
+
+    def AngleBetween(self, vector: Vector2D) -> float:
+        return math.degrees(math.acos(self.Dot(vector)/(self.Magnitude()*vector.Magnitude())))
 
     def DisplayVector(self) -> str:
         return f'({self.x}, {self.y})'
